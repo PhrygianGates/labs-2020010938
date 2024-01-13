b50999ffb3bd1ff1a3c7172407adc0da9ddd0952
Zhicheng Xiong <zhichengx@nvidia.com>
2024-01-13 11:59:11 +0800

    add missing file

---
+++ os/src/sync/utils.rs
@@ -0,0 +1,14 @@
+use alloc::{collections::VecDeque, sync::Arc};
+use crate::task::{TaskControlBlock};
+
+fn get_next_queue_id(wait_queue: &VecDeque<Arc<TaskControlBlock>>) -> isize {
+    if wait_queue.is_empty() {
+        -1
+    } else {
+        if let Some(waking_task) = wait_queue.front() {
+            waking_task.inner_exclusive_access().res.as_ref().unwrap().tid as isize
+        } else {
+            -1
+        }
+    }
+}
